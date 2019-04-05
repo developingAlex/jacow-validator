dc548dabb58b013871b4f6127458322c10427f66
Nathan Mudie <nathan@twish.com.au>
2019-04-05 18:46:50 +1100

    formatting and imports fixes for utils.py

--- src/jacowvalidator/utils.py
+++ src/jacowvalidator/utils.py
@@ -1,11 +1,9 @@
-from docx import Document
-from docx.shared import Inches, Mm
-from pprint import pprint
-
 import re
-from glob import glob
-from itertools import chain
 from collections import OrderedDict
+from itertools import chain
+
+from docx.shared import Inches, Mm
+
 
 def check_margins_A4(section):
     return get_margins_A4(section) == [37, 19, 20, 20]
@@ -72,7 +70,11 @@ def get_paragraph_style_exceptions(doc):
     jacow_styles = get_jacow_styles(doc)
     exceptions = []
     for i, p in enumerate(doc.paragraphs):
-        if not p.text.strip() == '' and p.style.name not in jacow_styles and p.style.name not in OTHER_VALID_STYLES:
+        if (
+            not p.text.strip() == ''
+            and p.style.name not in jacow_styles
+            and p.style.name not in OTHER_VALID_STYLES
+        ):
             exceptions.append(p)
     return exceptions
 
@@ -82,15 +84,19 @@ RE_REFS_INTEXT = re.compile(r'(?<!^)\[([\d ,-]+)\]')
 RE_FIG_TITLES = re.compile(r'(^Figure \d+:)')
 RE_FIG_INTEXT = re.compile(r'(Fig.\s?\d+|Figure\s?\d+\s+)')
 
+
 def _ref_to_int(ref):
     try:
-        return [int(ref), ]
+        return [int(ref)]
     except ValueError:
         if ',' in ref:
-            return list(chain.from_iterable(_ref_to_int(i) for i in ref.split(',') if i.strip()))
+            return list(
+                chain.from_iterable(_ref_to_int(i) for i in ref.split(',') if i.strip())
+            )
         elif '-' in ref:
-            return list(range(*(int(v)+i for i, v in enumerate(ref.split('-')))))
+            return list(range(*(int(v) + i for i, v in enumerate(ref.split('-')))))
         raise
+
 
 def extract_references(doc):
     data = iter(doc.paragraphs)
@@ -110,14 +116,12 @@ def extract_references(doc):
             references_in_text.append(_ref_to_int(ref))
 
         for ref in RE_REFS_LIST.findall(p.text.strip()):
-            references_list.append(dict(
-                id=int(ref),
-                text=p.text.strip(),
-                style=p.style.name,
-            ))
+            references_list.append(
+                dict(id=int(ref), text=p.text.strip(), style=p.style.name)
+            )
 
     # check refences in body are in correct order
-    stack = [0, ]
+    stack = [0]
     seen = []
     out_of_order = set()
     for _range in references_in_text:
@@ -135,7 +139,7 @@ def extract_references(doc):
         if len(seen) > 0:
             out_of_order.update(seen)
 
-     # get a set of references so we know which ones are used
+    # get a set of references so we know which ones are used
     used_references = set(chain.from_iterable(references_in_text))
 
     # check reference styles, order etc
@@ -151,11 +155,13 @@ def extract_references(doc):
                 ref['style_ok'] = ref['style'] == 'JACoW_Reference #1-9 when >= 10 Refs'
             else:
                 ref['style_ok'] = ref['style'] == 'JACoW_Reference #10 onwards'
-    
+
     return references_in_text, references_list
+
 
 def _fig_to_int(s):
     return int(''.join(filter(str.isdigit, s)))
+
 
 def extract_figures(doc):
     figures_refs = []
@@ -164,38 +170,45 @@ def extract_figures(doc):
     for p in doc.paragraphs:
         # find references to figures
         for f in RE_FIG_INTEXT.findall(p.text):
-            figures_refs.append(dict(
-                id=_fig_to_int(f),
-                name=f.strip()
-            ))
+            figures_refs.append(dict(id=_fig_to_int(f), name=f.strip()))
 
         # find figure captions
         for f in RE_FIG_TITLES.findall(p.text.strip()):
-            _id=_fig_to_int(f)
-            figures_captions.append(dict(
-                id=_id,
-                name=f,
-                text=p.text.strip(),
-                style=p.style.name,
-                style_ok=p.style.name in ['Figure Caption', 'Caption Multi Line'],
-            ))
+            _id = _fig_to_int(f)
+            figures_captions.append(
+                dict(
+                    id=_id,
+                    name=f,
+                    text=p.text.strip(),
+                    style=p.style.name,
+                    style_ok=p.style.name in ['Figure Caption', 'Caption Multi Line'],
+                )
+            )
 
     figures = OrderedDict()
-    _last = max(chain.from_iterable([(fig['id'] for fig in figures_captions), (fig['id'] for fig in figures_refs)]))
+    _last = max(
+        chain.from_iterable(
+            [
+                (fig['id'] for fig in figures_captions),
+                (fig['id'] for fig in figures_refs),
+            ]
+        )
+    )
 
-    for i in range(1, _last+1):
+    for i in range(1, _last + 1):
         caption = [c for c in figures_captions if c['id'] == i]
 
         figures[i] = {
             'refs': list(f['name'] for f in figures_refs if f['id'] == i),
             'duplicate': len(caption) != 1,
-            'found': len(caption) > 0
+            'found': len(caption) > 0,
         }
         figures[i]['used'] = len(figures[i]['refs']) > 0
         if caption:
             figures[i].update(**caption[0])
 
     return figures
+
 
 # These are in the jacow templates so may be in docs created from them
 # Caption and Normal for table title and figure title
