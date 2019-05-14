47decd014b47621b59d39922e685b45b1e48a6d3
Rosemary Waghorn <flyladyhawke@gmail.com>
2019-05-14 15:38:52 +1000

    remove logic for summary creation from routes to separate function

--- src/jacowvalidator/routes.py
+++ src/jacowvalidator/routes.py
@@ -9,29 +9,10 @@ from flask_uploads import UploadNotAllowed
 
 from jacowvalidator import app, documents
 from .models import Log
-from jacowvalidator.docutils.page import (check_tracking_on, get_abstract_and_author, TrackingOnError)
-from jacowvalidator.docutils.margins import check_sections
-from jacowvalidator.docutils.styles import check_jacow_styles
-from jacowvalidator.docutils.title import extract_title
-from jacowvalidator.docutils.references import extract_references
-from jacowvalidator.docutils.heading import get_headings
-from jacowvalidator.docutils.paragraph import get_paragraphs
-from jacowvalidator.docutils.figures import extract_figures
-from jacowvalidator.docutils.languages import (get_language_tags, get_language_tags_location, VALID_LANGUAGES)
-from jacowvalidator.docutils.doc import parse_paragraphs, parse_all_paragraphs, AbstractNotFoundError
-
-from jacowvalidator.docutils.tables import (
-    check_table_titles,
-)
-
-from .test_utils import (
-    replace_identifying_text,
-)
-
-from .spms import (
-    reference_csv_check,
-    PaperNotFoundError,
-)
+from jacowvalidator.docutils.page import (check_tracking_on, TrackingOnError)
+from jacowvalidator.docutils.doc import create_upload_variables, AbstractNotFoundError
+from .test_utils import replace_identifying_text
+from .spms import PaperNotFoundError
 
 
 try:
@@ -107,136 +88,13 @@ def upload():
 
         try:
             doc = Document(fullpath)
-            doc_summary = parse_paragraphs(doc)
             metadata = doc.core_properties
-            summary = {}
 
             # check whether tracking on
             result = check_tracking_on(doc)
 
-            # get style details
-            jacow_styles = check_jacow_styles(doc)
-            summary['Styles'] = {
-                'title': 'JACoW Styles',
-                'ok': all([tick['style_ok'] for tick in jacow_styles]),
-                'message': 'Styles issues',
-                'details': jacow_styles,
-                'anchor': 'styles'
-            }
-
-            # get page size and margin details
-            sections = check_sections(doc)
-            ok = all([tick['margins_ok'] for tick in sections]) and all([tick['col_ok'] for tick in sections])
-            summary['Margins'] = {
-                'title': 'Page Size and Margins',
-                'ok': ok,
-                'message': 'Margins',
-                'details': sections,
-                'anchor': 'pagesize'
-            }
-
-            language_summary = get_language_tags(doc)
-            languages = get_language_tags_location(doc)
-            summary['Languages'] = {
-                'title': 'Languages',
-                'ok': len([languages[lang] for lang in languages if languages[lang] not in VALID_LANGUAGES]) == 0,
-                'message': 'Language issues',
-                'details': language_summary,
-                'extra': languages,
-                'anchor': 'language'
-            }
-
-            # get parsed document summary of styles
-            all_summary = parse_all_paragraphs(doc)
-            ok = all([tick['style_ok'] is True for tick in all_summary])
-            if not ok:
-                ok = 2
-            summary['List'] = {
-                'title': 'Parsed Document',
-                'ok': ok,
-                'message': 'Not using only JACoW Styles',
-                'details': all_summary,
-                'anchor': 'list',
-                'showTotal': True,
-            }
-
-            summary['Title'] = doc_summary['Title']
-            title = doc_summary['Title']['details'][0]
-
-            summary['Authors'] = doc_summary['Authors']
-            authors = doc_summary['Authors']['details']
-
-            summary['Abstract'] = doc_summary['Abstract']
-            # summary['Headings'] = doc_summary['Headings']
-
-            headings = get_headings(doc)
-            summary['Headings'] = {
-                'title': 'Headings',
-                'ok': all([tick['style_ok'] is True for tick in headings]),
-                'message': 'Heading issues',
-                'details': headings,
-                'anchor': 'heading',
-                'showTotal': True,
-            }
-
-            paragraphs = get_paragraphs(doc)
-            summary['Paragraphs'] = {
-                'title': 'Paragraphs',
-                'ok': all([tick['style_ok'] for tick in paragraphs]),
-                'message': 'Paragraph issues',
-                'details': paragraphs,
-                'anchor': 'paragraph',
-                'showTotal': True,
-            }
-
-            references_in_text, references_list = extract_references(doc)
-            summary['References'] = {
-                'title': 'References',
-                'ok': references_list
-                      and all([tick['style_ok'] and tick['used_ok'] and tick['order_ok'] for tick in references_list]),
-                'message': 'Reference issues',
-                'details': references_list,
-                'anchor': 'references',
-                'showTotal': True,
-            }
-
-            figures = extract_figures(doc)
-            ok = True
-            for _, sub in figures.items():
-                ok = ok and all([item['caption_ok'] and item['used_ok'] and item['style_ok'] for item in sub])
-
-            summary['Figures'] = {
-                'title': 'Figures',
-                'ok': ok,
-                'message': 'Figure issues',
-                'details': figures,
-                'anchor': 'figures',
-                'showTotal': True,
-            }
-
-            table_titles = check_table_titles(doc)
-            summary['Tables'] = {
-                'title': 'Tables',
-                'ok': all([
-                    all([tick['text_format_ok'], tick['order_ok'], tick['style_ok'], tick['used'] > 0])
-                    for tick in table_titles]),
-                'message': 'Table issues',
-                'details': table_titles,
-                'anchor': 'tables',
-                'showTotal': True,
-            }
-
-            if "URL_TO_JACOW_REFERENCES_CSV" in os.environ:
-                reference_csv_url = os.environ["URL_TO_JACOW_REFERENCES_CSV"]
-                author_text = ''.join([a['text']+", " for a in authors])
-                reference_csv_details = reference_csv_check(paper_name, title['text'], author_text)
-                summary['SPMS'] = {
-                    'title': 'SPMS Abstract Title Author Check',
-                    'ok': reference_csv_details['title']['match'] and reference_csv_details['author']['match'],
-                    'message': 'SPMS Abstract Title Author Check issues',
-                    'details': reference_csv_details['summary'],
-                    'anchor': 'spms'
-                }
+            # get variables to pass to template
+            summary, reference_csv_details, title = create_upload_variables(doc, paper_name)
 
             # log = Log()
             # log.filename = filename
